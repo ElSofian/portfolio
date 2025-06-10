72acf9244851833486b7ec84daf731c08a75c846
Sofian <sofian.developpeur@gmail.com>
2025-06-10 18:40:10 +0200

    copy email instead of redirect

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Home() {
 
+	const handleCopyMail = () => {
+		navigator.clipboard.writeText("contact@sofianelaloui.me");
+		toast.success("Email copié avec succès !");
+	}
+
 	return (
+		<>
+		<ToastContainer position="top-center" autoClose={2000} />
 		<div className="flex flex-col items-center overflow-y-auto dark:text-gray-100">
 			<div className="p-10">
 				<div className="flex flex-col gap-8 font-medium text-xl w600:text-lg w600:p-[30px]">
@@ -10,17 +18,16 @@ export default function Home() {
 					<p>Explorez mes projets pour voir comment j&apos;utilise les technologies modernes afin de créer des solutions performantes et ergonomiques.</p>
 					<div className="flex items-center gap-8">
 						<Link href="https://github.com/ElSofian" target="_blank">
-							<i className="fa-brands fa-github fa-lg" />
+							<i className="fa-brands fa-github fa-lg cursor-click" />
 						</Link>
 						<Link href="https://www.linkedin.com/in/sofian-elaloui/" target="_blank">
-							<i className="fa-brands fa-linkedin fa-lg" />
+							<i className="fa-brands fa-linkedin fa-lg cursor-click" />
 						</Link>
-						<Link href="mailto:contact@sofianelaloui.me" target="_blank">
-							<i className="fa-solid fa-envelope fa-lg" />
-						</Link>
+						<i className="fa-solid fa-envelope fa-lg cursor-click" onClick={() => handleCopyMail()} />
 					</div>
 				</div>
 			</div>
 		</div>
+		</>
 	)
 }
