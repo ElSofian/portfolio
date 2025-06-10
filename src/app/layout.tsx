6d215a398f3aa5d949ebfc120d42dfac81e8783e
Sofian <sofian.developpeur@gmail.com>
2025-06-10 18:26:01 +0200

    edit description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Sofian's Portfolio",
-  description: "Sofian's Portfolio is a website where you can see my projects. Btw, i'm searching for an Alternance !",
+  description: "Sofian's Portfolio is a place where you can see my projects. Btw, i'm searching for an Alternance !",
 };
 
 export default function RootLayout({
